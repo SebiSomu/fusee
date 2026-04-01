459815e5678b48d281372de8f07625c64952dd8c
SebiSomu <sebigsomu@gmail.com>
2026-04-01 21:22:10 +0300

    Fixed issues with types of mountTemplate and internal function

--- framework/types/signal.d.ts
+++ framework/types/signal.d.ts
@@ -12,15 +12,16 @@ export type ComputedAccessor<T> = {
 export type Signal<T = any> = SignalAccessor<T>
 export type Computed<T = any> = ComputedAccessor<T>
 
-export type EffectRunner = {
+type EffectRunner = {
     (): void;
     deps: Set<Set<EffectRunner>>;
 }
 
 export declare function signal<T>(initialValue: T): Signal<T>
 export declare function effect(fn: () => void): () => void
-export declare function setEffectHook(fn: (eff: any) => void): void
 export declare function computed<T>(fn: () => T): Computed<T>
 export declare function batch<T>(fn: () => T): T
 export declare function untrack<T>(fn: () => T): T
+
+declare function setEffectHook(fn: (eff: any) => void): void
 
